f32a3edac453ede46bcff6562886613e482e162b
Luca De Camillis <lucadecamillis@gmail.com>
2022-11-05 13:40:43 +0100

    Create domain list

---
+++ src/Dmb.Model/Base/DomainList.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using Dmb.Model.Interfaces;
+
+namespace Dmb.Model.Base;
+
+public class DomainList<T> : IReadOnlyList<T>
+{
+    protected readonly IDomainBehavior behavior;
+
+    private readonly IList<T> inner;
+
+    #region  CTOR
+
+    public DomainList(IDomainBehavior behavior)
+        : this(new List<T>(), behavior) { }
+
+    public DomainList(IList<T> list, IDomainBehavior behavior)
+    {
+        this.behavior = behavior;
+        this.inner = list;
+    }
+
+    #endregion
+
+    #region IReadOnlyList
+
+    public T this[int index]
+    {
+        get { return this.inner[index]; }
+    }
+
+    public int Count
+    {
+        get { return this.inner.Count; }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return this.inner.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.inner.GetEnumerator();
+    }
+
+    #endregion
+}
