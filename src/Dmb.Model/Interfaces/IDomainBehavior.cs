9f0b8b1a9e57d147215e5a7557ee0d2c9bad4739
Luca De Camillis <lucadecamillis@gmail.com>
2022-11-25 17:17:19 +0100

    Docs

--- src/Dmb.Model/Interfaces/IDomainBehavior.cs
+++ src/Dmb.Model/Interfaces/IDomainBehavior.cs
@@ -5,17 +5,39 @@ namespace Dmb.Model.Interfaces;
 
 public interface IDomainBehavior
 {
+    /// <summary>
+    /// Add an entity to the domain model
+    /// </summary>
+    /// <param name="entity"></param>
     void AddEntity(
         EntityBase entity);
 
+    /// <summary>
+    /// Remove an entity from the domain model
+    /// </summary>
+    /// <param name="entity"></param>
     void RemoveEntity(
         EntityBase entity);
 
+    /// <summary>
+    /// Set a property of an entity in the domain model
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="storage"></param>
+    /// <param name="value"></param>
+    /// <param name="propertyName"></param>
+    /// <returns></returns>
     bool SetProperty<T>(
         ref T storage,
         T value,
         [CallerMemberName] string? propertyName = null);
 
+    /// <summary>
+    /// Add an item to a domain collection
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="collection"></param>
+    /// <param name="item"></param>
     void AddToCollection<T>(
         ICollection<T> collection,
         T item);
